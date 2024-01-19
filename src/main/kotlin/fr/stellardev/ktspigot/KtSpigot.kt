ab9b7fde3a10a8801de5d8a0ba98b4a0b94adb13
StellarFX <priveeofficiel@gmail.com>
2024-01-19 11:13:25 +0100

    🎉 init(project): initial commit

---
+++ src/main/kotlin/fr/stellardev/ktspigot/KtSpigot.kt
@@ -0,0 +1,11 @@
+package fr.stellardev.ktspigot
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KtSpigot: JavaPlugin() {
+
+        override fun onEnable() {
+            logger.info("Plugin is enabled!")
+        }
+
+}
